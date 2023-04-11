2ac04bb733c8258c4b7eb4bfac539ebb58ecbe05
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-11 10:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+function firstNonRepeatingChar(str) {
+	const charCount = {};
+	for (let i = 0; i < str.length; i++) {
+		const char = str[i]
+		charCount[char] = charCount[char] === 1 ? charCount[char] + 1 : 1;
+	}
+	for(let key in charCount){
+		if (charCount[key] === 1) {
+			return key;
+		}
+	}
+	return null;
+}
+
+let str = prompt("Enter a string");
+alert(firstNonRepeatingChar(str));
